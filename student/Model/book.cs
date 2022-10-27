c93d31e1c7752d112d43772032bf2637b490a499
AAAZ <AAAZ@CYBERSPACE>
2022-10-27 21:40:46 +0500

    Add project files.

---
+++ student/Model/book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace student.Model
+{
+    public class book
+    {
+        [JsonPropertyName("id")]
+        public string book_image { get; set; }
+        [JsonPropertyName("name")]
+        public string book_name { get; set; }
+        [JsonPropertyName("image")]
+        public string book_id { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<book>(this);
+        }
+    }
+}
